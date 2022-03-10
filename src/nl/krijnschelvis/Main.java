dc1113e015e3c22edce795a03a867c5601a03dc2
Superruubje <gj.vermeulen@hotmail.nl>
2022-03-10 14:59:18 +0100

    class gebruiker

--- src/nl/krijnschelvis/Main.java
+++ src/nl/krijnschelvis/Main.java
@@ -1,6 +1,7 @@
 package nl.krijnschelvis;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -19,10 +20,20 @@ class Gebruiker{
     private ArrayList<String> examensGehaalt;
     private ArrayList<String> pogingen;
 
+
     public Gebruiker(String name, int studentenNummer, ArrayList<String> examensGehaalt, ArrayList<String> pogingen) {
         this.name = name;
         this.studentenNummer = studentenNummer;
         this.examensGehaalt = examensGehaalt;
         this.pogingen = pogingen;
     }
+
+    public static void AddStudent(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "1) Vul je gegevens in\n" +
+                        "2) Exit");
+    }
+
+
 }
